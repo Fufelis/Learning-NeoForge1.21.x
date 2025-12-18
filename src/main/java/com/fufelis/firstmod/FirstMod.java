0a69449b0b39f4f95ac23b0c6c42d4453b56aee1
Fufelis <raimonds94@gmail.com>
2025-12-19 00:39:04 +0200

    added custom blocks and items

--- src/main/java/com/fufelis/firstmod/FirstMod.java
+++ src/main/java/com/fufelis/firstmod/FirstMod.java
@@ -1,5 +1,7 @@
 package com.fufelis.firstmod;
 
+import com.fufelis.firstmod.block.ModBlocks;
+import com.fufelis.firstmod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -37,7 +39,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(FirstMod.MODID)
 public class FirstMod {
-    public static final String MODID = "fufelisfirstmod";
+    public static final String MODID = "firstmod";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public FirstMod(IEventBus modEventBus, ModContainer modContainer) {
@@ -50,6 +52,8 @@ public class FirstMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -64,7 +68,14 @@ public class FirstMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+            event.accept(ModBlocks.BISMUTH_ORE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
